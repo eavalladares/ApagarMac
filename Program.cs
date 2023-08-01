be54340f003de5d1eae943dd7a9a88b64d9dc500
Alexander Valladares <edwin.valladares23@itca.edu.sv>
2023-08-01 08:31:43 -0600

    Codigo apagar PC
    Este codigo permite apagar una pc ingresando por teclado la cantidad en minutos y tambien permite cancelar el proceso.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace ShutdownTimerMac
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese cantidad de minutos para realizar apagado");
+            int minutos = int.Parse(Console.ReadLine());
+
+            
+            int segundos = minutos * 60;
+
+            
+            Process.Start("sudo", $"shutdown -h +{segundos}");
+
+            Console.WriteLine($"La computadora se apagará en {minutos} minutos.");
+            Console.WriteLine("Ingrese 'C' para cancelar el apagado.");
+
+            
+            string input = Console.ReadLine();
+            if (input.ToLower() == "C")
+            {
+                
+                Process.Start("sudo", "killall shutdown");
+                Console.WriteLine("El apagado ha sido cancelado.");
+            }
+        }
+    }
+
+}
